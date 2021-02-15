38017ec5a0b0b30d40e6563deec727eedff83f61
beatricemachalica <77323123+beatricemachalica@users.noreply.github.com>
2021-02-15 12:08:59 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 // On utilise ECMAScript 2015 ES6
+document.querySelector('.burger').addEventListener("click", function (){
+    document.querySelector("nav").classList.toggle("visible");
+});
+
+// fonctionnement :
+
+// dans le document, je recherche le selecteur avec la class burger ; puis écouter les événements clic
+// lorsqu'il "entend" le clic, il execute une fonction
+// dans le doc, on recherche le selecteur "nav", on regarde sa liste de classe, et on lui demande
+// d'alterner entre ajouter/supprimer la classe "visible"
+// toggle = alterner un état vers un autre
