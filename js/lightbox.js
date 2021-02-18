9aaf193309bec147e308a09b5cf35ce03db78bee
beatricemachalica <beatka.macha68@yahoo.fr>
2021-02-18 15:41:22 +0100

    patch update

--- js/lightbox.js
+++ js/lightbox.js
@@ -26,27 +26,70 @@ document.querySelector("#lightClose").addEventListener("click", function () {
 
 function lightboxOn(elem) {
   let currentElem = elem,
-    prevElem = elem.previousElementSibling,
-    nextElem = elem.nextElementSibling;
+    prevElem = currentElem.previousElementSibling,
+    nextElem = currentElem.nextElementSibling;
   // ci-dessus je stock dans des variables l'element figure courant, suivant et précedent.
 
   // si le voisin précédent est null
   if (prevElem === null) {
-    prevElem = elem.parentElement.lastElementChild;
+    prevElem = currentElem.parentElement.lastElementChild;
   }
 
   // si le voisin suivant est null
   if (nextElem === null) {
-    nextElem = elem.parentElement.firstElementChild;
+    nextElem = currentElem.parentElement.firstElementChild;
   }
 
   // on récupère le contenu du src, du premier enfant img de la figure sur laquelle on clique
-  let src = elem.children[0].src;
+  let src = currentElem.children[0].src;
 
   // on récupère le src de l'img du voisin précédent
   let prevSrc = prevElem.children[0].src;
   // on récupère le src de l'img du voisin suivant
   let nextSrc = nextElem.children[0].src;
+
+  // **********************************************
+  // Slider : left arrow & right arrow
+
+  // au clique sur arrow left
+  document.querySelector("#lightLeft").addEventListener("click", function () {
+    // on récupère la figure précédente de celui du current
+    prevElem = currentElem.prevElementSibling;
+    // si le voisin précédent est null
+    if (prevElem === null) {
+      prevElem = currentElem.parentElement.lastElementChild;
+    }
+    while (prevElem.style.display === "none") {
+      currentElem = prevElem;
+      prevElem = currentElem.prevElementSibling;
+    }
+    currentElem = prevElem;
+    // on récupère l'adresse de l'image à afficher
+    let src = prevElem.children[0].src;
+
+    // on modifie le contenu de lightImage par le contenu du voisin précédent
+    lightImage.innerHTML = `<img src="${src}"/>`;
+  });
+
+  // au clique sur arrow right
+  document.querySelector("#lightRight").addEventListener("click", function () {
+    // on récupère la figure suivante de celui du current
+    nextElem = currentElem.nextElementSibling;
+    // si le voisin suivant est null
+    if (nextElem === null) {
+      nextElem = currentElem.parentElement.firstElementChild;
+    }
+    while (nextElem.style.display === "none") {
+      currentElem = nextElem;
+      nextElem = currentElem.nextElementSibling;
+    }
+    currentElem = nextElem;
+    // on récupère l'adresse de l'image à afficher
+    let src = nextElem.children[0].src;
+
+    // on modifie le contenu de lightImage par le contenu du voisin suivant
+    lightImage.innerHTML = `<img src="${src}"/>`;
+  });
 
   // on change les pptées CSS pour faire apparaitre la lightbox
   lightbox.style.zIndex = 100;
@@ -59,30 +102,6 @@ function lightboxOn(elem) {
   // on inclus notre image dans la lightbox
   document.querySelector("#lightImage").innerHTML = `<img src="${src}" />`;
 }
-
-// **********************************************************************************************
-// Slider
-
-// au clique sur arrow right
-
-document.querySelector("#lightRight").addEventListener("click", function () {
-  // on récupère la figure suivante de celui du current
-  nextElem = currentElem.nextElementSibling;
-  // si le voisin suivant est null
-  if (nextElem === null) {
-    nextElem = currentElem.parentElement.firstElementChild;
-  }
-  while (nextElem.style.display === "none") {
-    currentElem = nextElem;
-    nextElem = currentElem.nextElementSibling;
-  }
-  currentElem = nextElem;
-  // on récupère l'adresse de l'image à afficher
-  let src = nextElem.children[0].src;
-
-  // on modifie le contenu de lightImage par le contenu du voisin suivant
-  lightImage.innerHTML = `<img src="${src}"/>`;
-});
 
 // **********************************************************************************************
 // Tri de la galerie
