80a2c776f6a11dddde6bf07e13032b753f65ae12
beatricemachalica <beatka.macha68@yahoo.fr>
2021-02-18 18:29:35 +0100

    patch update lightbox

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,116 +1,136 @@
-// gallery
+// Gallery
+// On récupère toutes les figures dans un tableau
 const figures = document.querySelectorAll(".gallery-items figure");
 let lightbox = document.querySelector(".lightbox");
 
+// Pour chacune des figures dans l'array figures
 for (let figure of figures) {
+  // On lui attribue un event au click
   figure.addEventListener("click", function (e) {
-    // on supprime le comportement par défaut des <a>
+    console.log(this.dataset.gallery);
+    // on supprime le comportement par défaut pour éviter les rechargements de page
     e.preventDefault();
 
-    // on execute la fonction qui aura pour but d'ouvrir la lightbox avec
-    // le bon contenu et cette dernière récupère l'élément cliqué
+    // on execute la fonction qui aura pour but d'ouvrir la lightbox avec le bon contenu
+    // cette dernière récupère l'élément cliqué en paramètre (celui sur lequel on a mis l'écouteur d'évènement)
     lightboxOn(this);
   });
 }
 
-//pour activer la fermeture de la lightbox
+// Pour activer la fermeture de la lightbox
 document.querySelector("#lightClose").addEventListener("click", function () {
   lightbox.style.opacity = 0;
   lightbox.style.zIndex = -1;
   document.querySelector("#lightImage").innerHTML = "";
-  // ligne ci-dessus pour vider la place et dégager l'image qu'on vient de fermer
 });
+// ligne ci-dessus pour vider la place et dégager l'image qu'on vient de fermer
 
 // **********************************************************************************************
 // Lightbox
 
 function lightboxOn(elem) {
+  // On récupère l'élément figure précédent, le courant sur lequel on a cliqué et le suivant
   let currentElem = elem,
-    prevElem = currentElem.previousElementSibling,
-    nextElem = currentElem.nextElementSibling;
-  // ci-dessus je stock dans des variables l'element figure courant, suivant et précedent.
+    lightImage = document.querySelector("#lightImage"),
+    currentSrc = currentElem.children[0].src;
 
-  // si le voisin précédent est null
-  if (prevElem === null) {
-    prevElem = currentElem.parentElement.lastElementChild;
-  }
-
-  // si le voisin suivant est null
-  if (nextElem === null) {
-    nextElem = currentElem.parentElement.firstElementChild;
-  }
-
-  // on récupère le contenu du src, du premier enfant img de la figure sur laquelle on clique
-  let src = currentElem.children[0].src;
-
-  // on récupère le src de l'img du voisin précédent
-  let prevSrc = prevElem.children[0].src;
-  // on récupère le src de l'img du voisin suivant
-  let nextSrc = nextElem.children[0].src;
-
-  // **********************************************
-  // Slider : left arrow & right arrow
-
-  // au clique sur arrow left
+  // *** Au clique sur #left ***
   document.querySelector("#lightLeft").addEventListener("click", function () {
-    // on récupère la figure précédente de celui du current
+    // On récupère le figure précédent de celui du current
     prevElem = currentElem.previousElementSibling;
-    // si le voisin précédent est null
+    // Si le voisin précédent est null
     if (prevElem === null) {
       prevElem = currentElem.parentElement.lastElementChild;
     }
-    // while (prevElem.style.display === "none") {
-    //   currentElem = prevElem;
-    //   prevElem = currentElem.prevElementSibling;
-    // }
-    currentElem = prevElem;
-    // on récupère l'adresse de l'image à afficher
-    let src = prevElem.children[0].src;
 
-    // on modifie le contenu de lightImage par le contenu du voisin précédent
-    lightImage.innerHTML = `<img src="${src}"/>`;
+    while (prevElem.style.display === "none") {
+      currentElem = prevElem;
+      prevElem = currentElem.previousElementSibling;
+      if (prevElem === null) {
+        prevElem = currentElem.parentElement.lastElementChild;
+      }
+    }
+    currentElem = prevElem;
+
+    // On récupère l'adresse de l'image à afficher
+    let src = currentElem.children[0].src;
+
+    // On modifie le contenu de lightImage par le contenu du voisin précédent
+    lightImage.innerHTML = `<img src="${src}" />`;
   });
 
-  // au clique sur arrow right
+  // *** Au clique sur #right ***
   document.querySelector("#lightRight").addEventListener("click", function () {
-    // on récupère la figure suivante de celui du current
+    // On récupère le figure suivant de celui du current
     nextElem = currentElem.nextElementSibling;
-    // si le voisin suivant est null
+    // Si le voisin suivant est null
     if (nextElem === null) {
       nextElem = currentElem.parentElement.firstElementChild;
     }
-    // while (nextElem.style.display === "none") {
-    //   currentElem = nextElem;
-    //   nextElem = currentElem.nextElementSibling;
-    // }
-    currentElem = nextElem;
-    // on récupère l'adresse de l'image à afficher
-    let src = nextElem.children[0].src;
 
-    // on modifie le contenu de lightImage par le contenu du voisin suivant
-    lightImage.innerHTML = `<img src="${src}"/>`;
+    while (nextElem.style.display === "none") {
+      currentElem = nextElem;
+      nextElem = currentElem.nextElementSibling;
+      if (nextElem === null) {
+        nextElem = currentElem.parentElement.firstElementChild;
+      }
+    }
+    currentElem = nextElem;
+    // On récupère l'adresse de l'image à afficher
+    let src = nextElem.children[0].src;
+    // On modifie le contenu de lightImage par le contenu du voisin suivant
+    lightImage.innerHTML = `<img src="${src}" />`;
+    // on changera comme "actif" non plus l'image cliqué mais le figure suivant
+    currentElem = nextElem;
   });
 
-  // on change les pptées CSS pour faire apparaitre la lightbox
+  // On change les pptés CSS pour faire apparaitre la lightbox
   lightbox.style.zIndex = 100;
   lightbox.style.opacity = 1;
 
-  // on fait apparaitre la lightbox avec l'image complétée
-  // document.querySelector('body').innerHTML +=
-  // document.querySelector('#light')
-
-  // on inclus notre image dans la lightbox
-  document.querySelector("#lightImage").innerHTML = `<img src="${src}" />`;
+  // On inclus notre image dans la lightbox
+  document.querySelector(
+    "#lightImage"
+  ).innerHTML = `<img src="${currentSrc}" />`;
 }
 
-// **********************************************************************************************
-// Tri de la galerie
+/*** TRI DE LA GALLERY ***/
+document.querySelector("#tri-image").addEventListener("click", function (e) {
+  e.preventDefault();
+  for (let figure of figures) {
+    if (figure.dataset.gallery !== "image") {
+      figure.style.display = "none";
+    } else {
+      figure.style.display = "block";
+    }
+  }
+});
 
-// document.querySelector("#tri-image").addEventListener("click", function (e) {
-//   e.preventDefault();
-//   for (let figure of figures) {
-//     if (figure.dataset.gallery !== "image") {
-//       figure.style.display;
-//     }
-//   }
-// });
+document.querySelector("#tri-audio").addEventListener("click", function (e) {
+  e.preventDefault();
+  for (let figure of figures) {
+    if (figure.dataset.gallery !== "audio") {
+      figure.style.display = "none";
+    } else {
+      figure.style.display = "block";
+    }
+  }
+});
+
+document.querySelector("#tri-video").addEventListener("click", function (e) {
+  e.preventDefault();
+  for (let figure of figures) {
+    if (figure.dataset.gallery !== "video") {
+      figure.style.display = "none";
+    } else {
+      figure.style.display = "block";
+    }
+  }
+});
+
+document.querySelector("#tri-all").addEventListener("click", function (e) {
+  e.preventDefault();
+  for (let figure of figures) {
+    figure.style.display = "block";
+  }
+});
