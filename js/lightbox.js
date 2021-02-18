6c310d849cd01dc1b4b1921f0995172af88b11c4
beatricemachalica <beatka.macha68@yahoo.fr>
2021-02-18 11:51:13 +0100

    gallery & slider

---
+++ js/lightbox.js
@@ -0,0 +1,36 @@
+// gallery
+const figures = document.querySelectorAll('.galerie figure');
+
+for(let figure of figures){
+  figure.addEventListener('click',function(e){
+    // on supprime le comportement par défaut des <a>
+    e.preventDefault();
+
+    // on execute la fonction qui aura pour but d'ouvrir la lightbox avec
+    // le bon contenu et cette dernière récupère l'élément cliqué 
+    lightbox(this);
+  });
+}
+function lightbox(elem){
+  // on récupère le contenu du src, du premier enfant img de la figure sur laquelle on clique
+  Let src = elem.children[0].src;
+
+  // on fait apparaitre la lightbox avec l'image complétée
+  document.querySelector('body').innerHTML += 
+  document.querySelector('#light')
+}
+
+function lightboxOn(elem){
+  Let currentElem = elem;
+}
+
+// Tri de la galerie
+
+document.querySelector('#tri-image').addEventListener('click',function(e){
+  e.preventDefault();
+  for(let figure of figures){
+    if(figure.dataset.gallery !== 'image'){
+      figure.style.display
+    }
+  }
+})
