3f91a3ab0733d3a75ff9543f196bb105d7e60d1f
beatricemachalica <beatka.macha68@yahoo.fr>
2021-02-18 14:25:13 +0100

    pour toi terence :)

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,5 +1,6 @@
 // gallery
 const figures = document.querySelectorAll('.gallery-items figure');
+Let lightbox = document.querySelector('.lightbox');
 
 for(let figure of figures){
   figure.addEventListener('click',function(e){
@@ -8,23 +9,54 @@ for(let figure of figures){
 
     // on execute la fonction qui aura pour but d'ouvrir la lightbox avec
     // le bon contenu et cette dernière récupère l'élément cliqué 
-    lightbox(this);
+    lightboxOn(this);
   });
 }
 
-function lightbox(elem){
+function lightboxOn(elem){
   // on récupère le contenu du src, du premier enfant img de la figure sur laquelle on clique
   Let src = elem.children[0].src;
-  Let lightbox = document.querySelector('.lightbox');
+
+  // on change les pptées CSS pour faire apparaitre la lightbox
+  lightbox.style.zIndex = 100;
+  lightbox.style.opacity = 1;
 
   // on fait apparaitre la lightbox avec l'image complétée
-  document.querySelector('body').innerHTML += 
-  document.querySelector('#light')
+  // document.querySelector('body').innerHTML +=
+  // document.querySelector('#light')
+
+  document.querySelector('#lightImage').innerHTML = `<img src"${src}" />`;
+  
+  lightboxClose();
 }
 
-function lightboxOn(elem){
-  Let currentElem = elem;
+function lightboxClose(){
+  document
+  .querySelector('#lightClose')
+  .addEventListener('clicke',function(){
+    lightbox.style.opacity = -1;
+    lightbox.style.zIndex = 0;
+  })
 }
+
+// au clique sur arrow right
+document.querySelector('#lightRight').addEventListener('click',function(){
+  // on récupère la figure suivante de celui du current
+  nextElem = currentElem.nextElementSibling;
+  // si le voisin suivant est null
+  if (nextElem === null){
+    nextElem = currentElem.parentElement.firstElementChild;
+  }
+  while (nextElem.style.display === 'none'){
+    currentElem = nextElem;
+    nextElem = currentElem.nextElementSibling;
+  }
+  currentElem = nextElem;
+  // on récupère l'adresse de l'image à afficher
+  Let src = nextElem.children[0].src;
+  // on modifie le contenu de lightImage par le contenu du voisin suivant
+  lightImage.innerHTML = `<img src="${src}"/>`;
+})
 
 // Tri de la galerie
 
